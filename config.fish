d848a9087d2e47380f69b6e2ed3e2750f359ba8e
sebcbi1 <sebcbi1@gmail.com>
2015-01-31 21:27:38 +0200

    first commit

---
+++ config.fish
@@ -0,0 +1,30 @@
+set fish_color_root red
+set fish_color_cwd white
+
+set __fish_git_prompt_show_informative_status 1
+set __fish_git_prompt_hide_untrackedfiles 1
+set __fish_git_prompt_showupstream "informative"
+set __fish_git_prompt_char_stateseparator ' -'
+set __fish_git_prompt_color_branch yellow --bold
+set __fish_git_prompt_describe_style default
+
+set __fish_git_prompt_char_stashstate  ' ↩'
+set __fish_git_prompt_char_upstream_ahead "↑"
+set __fish_git_prompt_char_upstream_behind "↓"
+set __fish_git_prompt_char_upstream_prefix ""
+
+set __fish_git_prompt_char_stagedstate " →"
+set __fish_git_prompt_color_stagedstate yellow --bold
+
+set __fish_git_prompt_char_dirtystate " ⚡"
+set __fish_git_prompt_color_dirtystate red --bold
+
+set __fish_git_prompt_char_untrackedfiles " …"
+set __fish_git_prompt_color_untrackedfiles red --bold
+
+set __fish_git_prompt_char_conflictedstate " ✖"
+set __fish_git_prompt_color_untrackedfiles red --bold
+set __fish_git_prompt_color_invalidstate red
+
+set __fish_git_prompt_char_cleanstate " ✔"
+set __fish_git_prompt_color_cleanstate green --bold
